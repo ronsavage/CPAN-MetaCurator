33e17fcf95a62ca2073bfe73ad1c0ab336de3221
Ron Savage <ron@savage.net.au>
2025-07-13 13:32:48 +1000

    Update re for module names versus paras

--- lib/CPAN/MetaCurator/Util/Export.pm
+++ lib/CPAN/MetaCurator/Util/Export.pm
@@ -90,7 +90,7 @@ sub format_text
 	my(@text)					= grep{length} split(/\n/, $text);
 	@text						= map{s/^-\s+//; s/:$//; $_} @text;
 	my($inside_see_also)		= false;
-	my($module_name_re)			= qr/^([A-Z]+[a-z]{0,}[A-Z]{0,}|[a-z]+)/o; # A Perl module, hopefully.
+	my($module_name_re)			= qr/^([A-Z]+[a-z]{0,}|[a-z]+)/o; # A Perl module, hopefully.
 
 	my($href);
 	my(@lines);
@@ -182,7 +182,7 @@ sub format_text
 		{
 			$$token{text} .= "<a href = '$_'>$_</a>";
 		}
-		elsif ($_ =~ $module_name_re) # Eg: builtins, Imager, GD and GD::Polyline.
+		elsif ( ($_ =~ $module_name_re) && (! $text_is_para) ) # Eg: builtins, Imager, GD and GD::Polyline. Not ChartingAndPlotting.
 		{
 			$$token{text} .= "<a href = 'https://metacpan.org/pod/$_'>$_</a>";
 		}
