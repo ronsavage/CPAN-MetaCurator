e6b977f5af19a4da75e1cb82474b48199e1ef747
Ron Savage <ron@savage.net.au>
2026-02-05 17:41:32 +1100

    Re-introduce packages table

--- scripts/build.db.sh
+++ scripts/build.db.sh
@@ -4,7 +4,7 @@ cd $HOME/perl.modules/CPAN-MetaCurator/
 scripts/drop.tables.pl
 scripts/create.tables.pl
 time scripts/populate.sqlite.tables.pl
-time scripts/export.as.tree.pl
+#time scripts/export.as.tree.pl
 
 declare -x SOURCE=html/cpan.metacurator.tree.html
 declare -x DEST=$DH/misc
