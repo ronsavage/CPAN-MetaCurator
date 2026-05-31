89af5e982c3352fade36343ac126a7c03b9829eb
Ron Savage <ron@savage.net.au>
2026-05-31 13:17:00 +1000

    Clean up See Also

--- lib/CPAN/MetaCurator/Database.pm
+++ lib/CPAN/MetaCurator/Database.pm
@@ -330,7 +330,7 @@ sub read_metapackager_table
 	my($sql)						= "select * from $table_name";
 	my($set)						= $self -> metapackager_db -> query($sql) || die $self -> metapackager_db -> error;
 	$set							= [$set -> hashes];
-	$$pad{count}{known}				= $#$set + 1;
+	$$pad{count}{$table_name}		= $#$set + 1;
 	$$pad{$table_name}				= {};
 	$$pad{$table_name}{$$_{name} }	= $$_{id} for (@$set);
 
