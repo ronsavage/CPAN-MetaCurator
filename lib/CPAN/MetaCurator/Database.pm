70a5b2d3a1cd118464a2155a377e1abd3d513925
Ron Savage <ron@savage.net.au>
2026-06-11 14:43:27 +1000

    Rewrite parse in Export.pm

--- lib/CPAN/MetaCurator/Database.pm
+++ lib/CPAN/MetaCurator/Database.pm
@@ -302,7 +302,7 @@ sub read_table
 	my($sql)				= "select * from $table_name";
 	my($set)				= $self -> db -> query($sql) || die $self -> db -> error;
 
-	say $_ grep{/codigo/} @$set;
+	say Dumper($_) grep{/codigo/} @$set;
 
 	# Return an arrayref of hashrefs.
 
