abd171b6f99de90a266552aacb40e774f65aaa9c
Ron Savage <ron@savage.net.au>
2026-06-18 11:01:39 +1000

    Using Tree::DAG_Node for See also

--- lib/CPAN/MetaCurator/Export.pm
+++ lib/CPAN/MetaCurator/Export.pm
@@ -295,6 +295,10 @@ sub parse_topic
 			if ($inside{see_also})
 			{
 				push@see_also, $token;
+
+				$see_also = Tree::DAG_Node -> new({name => 'See also', attributes => {id => ++$leaf_id} });
+
+				$daughter -> add_daughter($see_also);
 			}
 			elsif ($line_count == 1)
 			{
