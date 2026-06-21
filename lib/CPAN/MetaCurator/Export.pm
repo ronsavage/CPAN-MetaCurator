12b6469c5de5e948b047eeba3c948dfe5046b2ce
Ron Savage <ron@savage.net.au>
2026-06-21 14:02:31 +1000

    Grafting See also into original wiki

--- lib/CPAN/MetaCurator/Export.pm
+++ lib/CPAN/MetaCurator/Export.pm
@@ -109,7 +109,7 @@ sub export_tree
 			{
 				push @list, qq|\t<li data-jstree='{"opened": false}' id = '$leaf_id'>See also|;
 				push @list, "\t<ul>";
-				push @list, "\t\t<li data-jstree='{"opened": false}' id = '$$_{id}'>$$_{text}</li>" for (@$see_also_ref);
+				push @list, qq|\t\t<li>$$_{text}</li>| for (@$see_also_ref);
 				push @list, "\t</ul>";
 				push @list, "\t</li>";
 			}
