3a2a5dae3098c6daffbc21130e00b68067952e27
Ron Savage <ron@savage.net.au>
2026-06-23 09:03:37 +1000

    Trying to fix encoding of various items

--- lib/CPAN/MetaCurator/Export.pm
+++ lib/CPAN/MetaCurator/Export.pm
@@ -355,14 +355,16 @@ sub parse_topic
 sub write_file
 {
 	my($self, $header, $body, $footer, $pad) = @_;
-	my($encoding)		= lc $$pad{encoding};
-	my($output_path)	= File::Spec -> catfile($self -> home_path, $self -> output_path);
+	my($output_path) = File::Spec -> catfile($self -> home_path, $self -> output_path);
 
-	open(my $fh, ">$encoding", $output_path);
+	# $$pad{encoding} has a : prefix, & the value is from the constants table, which is from
+	# /home/ron/perl.modules/CPAN-MetaCurator/data/cpan.metacurator.constants.csv.
+
+	open(my $fh, ">$$pad{encoding}", $output_path);
 	print $fh $header, $body, $footer;
 	close $fh;
 
-	$self -> logger -> info("Created $output_path. Encoding: $encoding");
+	$self -> logger -> info("Created $output_path. Encoding: $$pad{encoding}");
 
 } # End of write_file.
 
