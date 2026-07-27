46283df4edaa0a6d1fda228d3aa0d18ebb4f8f4c
Ron Savage <ron@savage.net.au>
2026-07-27 17:45:16 +1000

    Clean up stats in Export.pm

--- lib/CPAN/MetaCurator/Export.pm
+++ lib/CPAN/MetaCurator/Export.pm
@@ -86,7 +86,7 @@ sub build_dag_tree
 			$module				= $token;
 			$note_count			= 0;
 
-			say "name: $$topic{name}. title: $$topic{title}" if ($$topic{name} eq 'ACMA');
+			say "title: $$topic{title}. token: $token" if ($$topic{title} eq 'ACMA');
 
 			if (! $seen{$module} && ($$topic{title} ne 'Acronyms') )
 			{
