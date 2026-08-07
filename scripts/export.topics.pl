656731c6cc0813642d813ef2a7679947a8fc2a33
Ron Savage <ron@savage.net.au>
2026-08-07 15:12:18 +1000

    Export latest tiddlers.json as a tree

--- scripts/export.topics.pl
+++ scripts/export.topics.pl
@@ -1,6 +1,13 @@
 #!/usr/bin/env perl
 
 my($db_path)	= 'data/cpan.metacurator.sqlite';
-my($csv_path)	= '/tmp/topics.csv';
+my($log_path)	= '/tmp/topics.txt';
 
-`echo ".h on\n.mode csv\nselect * from modules order by id" | sqlite3 $db_path > $csv_path`;
+# To get \n recognized we need to pipe text into sqlite3 via `...`.
+
+`echo 'Table name: constants' > $log_path`;
+`echo ".headers on\n.mode csv\nselect id,name,value from constants order by id" | sqlite3 $db_path >> $log_path`;
+`echo 'Table name: modules' >> $log_path`;
+`echo ".headers on\n.mode csv\nselect id,name,timestamp from modules order by id" | sqlite3 $db_path >> $log_path`;
+`echo 'Table name: topics' >> $log_path`;
+`echo ".headers on\n.mode csv\nselect id,parent_id,title,text,timestamp from topics order by id" | sqlite3 $db_path >> $log_path`;
--- scripts/export.topics.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-declare -x SOURCE=data/cpan.metacurator.sqlite
-declare -x DEST=/tmp/topics.csv
-
-echo ".headers on\n.mode csv\nselect id,title from topics order by title" | sqlite3 $SOURCE > $DEST
-#echo ".mode csv\nselect id,title from topics order by title" | sqlite3 $SOURCE > $DEST
